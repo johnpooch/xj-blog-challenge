6a83c1047dd63b21ade626c3a0ca6cfe0c916196
John McDowell <johnmcdowell0801@gmail.com>
2018-06-22 14:47:27 +0000

    working app

--- xj_blog_challenge/settings.py
+++ xj_blog_challenge/settings.py
@@ -36,8 +36,12 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'django_forms_bootstrap',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'posts',
+    'accounts'
+    
 ]
 
 MIDDLEWARE = [
@@ -55,7 +59,7 @@ ROOT_URLCONF = 'xj_blog_challenge.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,10 +121,9 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
-
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- xj_blog_challenge/urls.py
+++ xj_blog_challenge/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from posts import urls as posts_urls
+from accounts import urls as accounts_urls
+from django.views.static import serve
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include(accounts_urls)),
+    path('', include(posts_urls)),
+    path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
