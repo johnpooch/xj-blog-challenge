4544507a23b8902ce735ff4ec81454e6bb8ac3aa
John McDowell <johnmcdowell0801@gmail.com>
2018-06-22 10:13:15 +0000

    previous databse

--- xj_blog_challenge/settings.py
+++ xj_blog_challenge/settings.py
@@ -74,16 +74,17 @@ WSGI_APPLICATION = 'xj_blog_challenge.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
-   'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
+
+# LATER:
+# DATABASES = {
+#   'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+# }
 
 
 # Password validation
